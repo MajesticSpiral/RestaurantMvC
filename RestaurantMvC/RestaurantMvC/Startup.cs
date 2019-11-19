4df0fd1b588bd79819cd0269fa2bcc54ee2d1386
Callum Cheshire <sftwgot@hotmail.com>
2019-11-19 00:24:39 +0000

    Setup design layout

---
+++ RestaurantMvC/RestaurantMvC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantMvC.Startup))]
+namespace RestaurantMvC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
