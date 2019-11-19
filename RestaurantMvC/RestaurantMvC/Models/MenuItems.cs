e495e69266571e0a9670078f1e878309021d88df
Callum Cheshire <sftwgot@hotmail.com>
2019-11-19 15:40:26 +0000

    Created MenuItem controller for the admin panel

---
+++ RestaurantMvC/RestaurantMvC/Models/MenuItems.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantMvC.Models
+{
+	[Bind(Exclude = "MenuItemId")]
+	public class MenuItems
+	{
+		[ScaffoldColumn(false)]
+		public int MenuItemId { get; set; }
+
+
+		[Required(ErrorMessage = "Price is required")]
+		[Range(0.01, 100.00,
+			ErrorMessage = "Price must be between 0.01 and 100.00")]
+		public decimal Price { get; set; }
+
+		
+
+		public virtual MenuItems MenuItem { get; set; }
+		public virtual List<MenuItems> OrderDetails { get; set; }
+	}
+}
